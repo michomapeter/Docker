80adc2b95a019d785283ae479322cb4f6a558b7c
Peter michoma <pmichoma@kabarak.ac.ke>
2023-02-02 11:31:17 -0500

    these is the docker server configurations

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/' ,(req,res) =>{
+  res.send('Backend dev');
+});
+
+app.listen(PORT , HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
